217fda4eec7b89642e26b2a531965933ad0be313
NYJP <39363839+NYJP@users.noreply.github.com>
2023-01-21 09:59:40 +0000

    making appt skeleton

--- BlogApp/views/appointments.py
+++ BlogApp/views/appointments.py
@@ -2,46 +2,50 @@ from flask import render_template, request, url_for, redirect, Blueprint, flash
 from werkzeug.exceptions import abort
 from BlogApp import db
 from BlogApp.forms import Login_client, Add_appointment, Add_doctor
+from flask_login import login_required, current_user
 
 appointments = Blueprint('appointments', __name__)
 
-@appointments.route("/",methods=['GET','POST'])
-def root():
-  return render_template('about.html')
-
 @appointments.route("/client_login",methods=['GET','POST'])
 def client_login():
-  return render_template('client_login.html',form = Login_client())
+  return render_template('appointments/client_login.html',form = Login_client())
 
 @appointments.route('/client_signup',methods=['GET','POST'])
 def client_signup():
-  return render_template('client_signup.html')
+  return render_template('appointments/client_signup.html')
 
 @appointments.route('/clinic',methods=['GET','POST'])
+@login_required
 def clinic():
-  return render_template('clinic.html')
+  return render_template('appointments/clinic.html')
 
 @appointments.route('/pharmacy',methods=['GET','POST'])
+@login_required
 def pharmacy():
-  return render_template('pharmacy.html')
+  return render_template('appointments/pharmacy.html')
 
-@appointments.route('/user_login',methods=['GET','POST'])
-def user_login():
-  return render_template('user_login.html')
+@appointments.route('/find_clinic',methods=['GET','POST'])
+#@login_required
+def find_c():
+  return render_template('appointments/find_clinic.html')
 
-@appointments.route('/user',methods=['GET','POST'])
-def user():
-  return render_template('user.html')
+@appointments.route('/find_doctor',methods=['GET','POST'])
+#@login_required
+def find_d():
+  return render_template('appointments/find_doctor.html')
 
-@appointments.route('/findslot',methods=['GET','POST'])
-def find():
-  return render_template('find_a_slot.html')
+@appointments.route('/find_slot',methods=['GET','POST'])
+#@login_required
+def find_s():
+  return render_template('appointments/find_slot.html')
 
 @appointments.route('/add_appointment',methods=['GET','POST'])
+#@login_required
 def add_a():
-  return render_template('add_appointment.html',form=Add_appointment())
+  return render_template('appointments/add_appointment.html',form=Add_appointment())
 
 @appointments.route('/add_doctor',methods=['GET','POST'])
+@login_required
 def add_d():
-  return render_template('add_doctor.html',form=Add_doctor())
+  return render_template('appointments/add_doctor.html',form=Add_doctor())
 
